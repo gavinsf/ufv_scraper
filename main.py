59cf600f663a33d878d94f72ddb10c690ca4e29f
Gavin.Foster <gavin.foster@student.ufv.ca>
2024-04-22 21:11:32 -0700

    adds main function and basic parser parts

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+
+def main():
+    url = "https://www.ufv.ca/arfiles/includes/202401-timetable-with-changes.htm"
+    page = urlopen(url)
+
+    html = page.read().decode("latin-1")
+    soup = BeautifulSoup(html, "html.parser")
+
+    spans = soup.find_all("span")
+    for row in spans:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
