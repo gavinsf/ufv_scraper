8b14566fecaf36ac1f55fef4c35546f10d35ddd5
Gavin.Foster <gsfoster031@gmail.com>
2024-05-07 14:17:13 -0700

    Moves row parsing to new function

--- main.py
+++ main.py
@@ -12,34 +12,35 @@ def main():
 
     splits = re.split(r'-----------------------------------------------------------------------------------------------------------------------------------', str(soup))
     for row in splits:
-        row = str(row)
-        '''
-        course = re.findall(r'[A-Z]+\s\d\d\d[A-Z]*', row)
-        if (len(course) != 0):
-            if (course[0].startswith("ABA") 
-                or course[0].startswith("ABB") 
-                or course[0].startswith("ABC") 
-                or course[0].startswith("ABD")
-                or course[0].startswith("ABE")
-                or course[0].startswith("ABF")
-                or course[0].startswith("ABH")
-                or course[0].startswith("CEPA")
-                or course[0].startswith("CEPV")):
-                course = []
-            else:
-                course = course[0]    
-        name = re.findall(r'(?<=\d\d\d\s\t)[A-Z].+(?=\s\d\.\d)', row)
-        ''' 
-
-        credits = re.findall(r'(?<=\s)[ 123456789]\.[05]', row)
-        if (len(credits) == 0):
-            credits = "No Credits"
-        elif (len(credits) >= 1):
-            credits = credits[0]
+        row_parser(row)
         
             
+def row_parser(row):
 
-        
+    row = str(row)
+'''
+    course = re.findall(r'[A-Z]+\s\d\d\d[A-Z]*', row)
+    if (len(course) != 0):
+        if (course[0].startswith("ABA") 
+            or course[0].startswith("ABB") 
+            or course[0].startswith("ABC") 
+            or course[0].startswith("ABD")
+            or course[0].startswith("ABE")
+            or course[0].startswith("ABF")
+            or course[0].startswith("ABH")
+            or course[0].startswith("CEPA")
+            or course[0].startswith("CEPV")):
+            course = []
+        else:
+            course = course[0]    
+    name = re.findall(r'(?<=\d\d\d\s\t)[A-Z].+(?=\s\d\.\d)', row)
+    credits = re.findall(r'(?<=\s)[ 123456789]\.[05]', row)
+    if (len(credits) == 0):
+        credits = "No Credits"
+    elif (len(credits) >= 1):
+        credits = credits[0]
+
+        '''         
 
 
 if __name__ == "__main__":
